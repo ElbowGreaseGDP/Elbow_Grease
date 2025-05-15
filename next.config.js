0f66e6f2fbb87c6304ead3041f5542889ad30ecc
danrokadia <D.Rokadia-20@student.lboro.ac.uk>
2025-05-15 14:23:57 +0100

    udpated base root to fix non-reference error

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 module.exports = {
-    // only in production exports do we use these
-    ...(isProd && {
-        output: 'export',
-        trailingSlash: true,
-        basePath: '/Elbow_Grease',
-        assetPrefix: '/Elbow_Grease/',
-        images: { unoptimized: true },
-    }),
-
-    // In dev mode we want plain localhost paths, so no basePath/assetPrefix
+    trailingSlash: true,
+    output: 'export',
+    images: { unoptimized: true },
 };
